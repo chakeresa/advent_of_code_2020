21402ac4187270b0b6eecd50a0aed100a18ec8fd
Alexandra Chakeres <alexchakeres@gmail.com>
2020-12-01 22:33:10 -0700

    Add problem descriptions

--- lib/day_1/sum_three_2020.ex
+++ lib/day_1/sum_three_2020.ex
@@ -1,4 +1,20 @@
 defmodule SumThree2020 do
+  @moduledoc """
+  The Elves in accounting are thankful for your help; one of them even offers
+  you a starfish coin they had left over from a past vacation. They offer you a
+  second one if you can find three numbers in your expense report that meet the
+  same criteria.
+
+  Using the example in `SumTwo2020` again, the three entries that sum to 2020
+  are 979, 366, and 675. Multiplying them together produces the answer,
+  241861950.
+
+  In your expense report, what is the product of the three entries that sum to
+  2020?
+
+  Your puzzle answer was 128397680.
+  """
+
   def solution do
     sorted = SumTwo2020.sorted_list()
 
