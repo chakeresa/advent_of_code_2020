bbcffc11b6218761423699dad631a3240e5af521
Alexandra Chakeres <alexchakeres@gmail.com>
2020-12-08 22:15:06 -0700

    Add daily templates

---
+++ lib/template/template.ex
@@ -0,0 +1,17 @@
+# defmodule Template do
+#   @list_of_lines_from_txt FileImport.list_of_lines("lib/day_9/data.txt")
+
+#   @doc """
+#   TODO
+#   """
+#   def part_1(list_of_lines \\ @list_of_lines_from_txt) do
+#     list_of_lines
+#   end
+
+#   @doc """
+#   TODO
+#   """
+#   def part_2(list_of_lines \\ @list_of_lines_from_txt) do
+#     list_of_lines
+#   end
+# end
